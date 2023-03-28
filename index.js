28ac2f61afaa6ec946e52f67b43451c26989c839
Saurabh8807 <sjagtap18092002@gmail.com>
2023-03-28 19:05:48 +0530

    changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('./db/config')
 const User = require('./db/reservations')
 const CardDetails = require('./db/cardDetails')
 const app = express()
+const PORT = process.env.PORT ||5000
 app.use(express.json())
 app.post('/Reservation',async(req,res)=>{
     let user = new User(req.body)
@@ -17,5 +18,5 @@ app.post('/Payment',async(req,res)=>{
     res.send(result)
 })
 app.use(cors())
-app.listen(5000)
+app.listen(PORT)
 
